a628276dbbefb2b433cb476f14ffe4d67976b443
m1tk <mohammed.salih2@usmba.ac.ma>
2024-10-22 16:43:12 +0100

    Add encrypt of files

--- demonstration.py
+++ demonstration.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QTextEdit, QLineEdit, QPushButton, QHBoxLayout, QMessageBox, QComboBox, QPushButton, QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QTextEdit, QLineEdit, QPushButton, QHBoxLayout, QMessageBox, QComboBox, QPushButton, QFileDialog, QTabWidget, QWidget
 import json
 from base64 import b64encode, b64decode
 from Crypto.Cipher import ChaCha20, ChaCha20_Poly1305
@@ -6,14 +6,32 @@ import os
 import hashlib
 from chacha import chacha20, xchacha20, chacha20poly1305
 import struct
+from functools import partial
 
-class EncryptionApp(QWidget):
+class EncryptionApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Encryption/Decryption App")
         
-        self.layout = QVBoxLayout()
+        self.setCentralWidget(InputEnc(self))
         
+class InputEnc(QWidget):
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+
+        self.lay = QVBoxLayout(self)
+
+        self.tabs = QTabWidget()
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+
+        self.lay.addWidget(self.tabs)
+
+        self.tabs.addTab(self.tab1,"Inputs")
+        self.tabs.addTab(self.tab2,"Files")
+
+        self.layout = QVBoxLayout(self)
+
         self.input_label = QLabel("Input:")
         self.layout.addWidget(self.input_label)
 
@@ -51,9 +69,9 @@ class EncryptionApp(QWidget):
         self.layout.addWidget(self.text_output)
 
         self.key_label = QLabel("Key:")
-        self.layout.addWidget(self.key_label)
+        self.lay.addWidget(self.key_label)
         self.key = QLineEdit()
-        self.layout.addWidget(self.key)
+        self.lay.addWidget(self.key)
         
         button_layout = QHBoxLayout()
         self.encrypt_button = QPushButton("Encrypt")
@@ -64,7 +82,7 @@ class EncryptionApp(QWidget):
         self.decrypt_button.clicked.connect(self.decrypt_text)
         button_layout.addWidget(self.decrypt_button)
 
-        self.layout.addLayout(button_layout)
+        self.lay.addLayout(button_layout)
         
         select_layout = QHBoxLayout()
 
@@ -78,10 +96,53 @@ class EncryptionApp(QWidget):
             "chacha20poly1305 (own implementation)"
         ])
         select_layout.addWidget(self.select_enc)
-        self.layout.addLayout(select_layout)
+        self.lay.addLayout(select_layout)
 
+        self.tab1.layout = self.layout
+        self.tab1.setLayout(self.tab1.layout)
+
+
+        self.layout1 = QVBoxLayout(self)
+        self.layout1.setSpacing(0)
+
+        self.input_label1 = QLabel("Input File:")
+        self.layout1.addWidget(self.input_label1)
+        self.in_file1 = QLabel("None")
+        self.layout1.addWidget(self.in_file1)
+
+        in_file_layout1 = QHBoxLayout()
+
+        self.input_load_btn1 = QPushButton('Load from file')
+        self.input_load_btn1.clicked.connect(partial(self.read_file, self.in_file1, False))
+        in_file_layout1.addWidget(self.input_load_btn1)
+
+        self.layout1.addLayout(in_file_layout1)
+
+        self.out_label1 = QLabel("Output File:")
+        self.layout1.addWidget(self.out_label1)
+        self.out_file1 = QLabel("None")
+        self.layout1.addWidget(self.out_file1)
+
+        out_file_layout1 = QHBoxLayout()
+
+        self.out_load_btn1 = QPushButton('Save to file')
+        self.out_load_btn1.clicked.connect(partial(self.read_file, self.out_file1, True))
+        out_file_layout1.addWidget(self.out_load_btn1)
+
+        self.layout1.addLayout(out_file_layout1)
+
+        self.tab2.layout = self.layout1
+        self.tab2.setLayout(self.tab2.layout)
         
-        self.setLayout(self.layout)
+        self.setLayout(self.lay)
+
+    def read_file(self, label, saver):
+        if saver:
+            file = self.show_file_saver_dialog()
+        else:
+            file = self.show_file_picker_dialog()
+        if file:
+            label.setText(file)
 
     def read_input_file(self):
         file = self.show_file_picker_dialog()
@@ -158,8 +219,20 @@ class EncryptionApp(QWidget):
         return hash_key.digest()
     
     def encrypt_text(self):
-        plaintext = self.text_input.toPlainText().encode()
-        key       = self.get_key()
+        if self.tabs.currentIndex() == 0:
+            plaintext = self.text_input.toPlainText().encode()
+        else:
+            try:
+                if not self.in_file1.text() == "None" and not self.out_file1.text() == "None":
+                    with open(self.in_file1.text(), 'rb') as file:
+                        plaintext = file.read()
+                else:
+                    return
+            except Exception as e:
+                self.show_error_message("Error", f"Failed loading file: {e}")
+
+
+        key = self.get_key()
         if plaintext:
             res = {}
             enc_type = self.select_enc.currentText()
@@ -190,21 +263,36 @@ class EncryptionApp(QWidget):
                 nonce      = os.urandom(12)
                 ciphertext = cipher.encrypt(nonce, plaintext, aad=b"header")
 
-
-            res["nonce"] = b64encode(nonce).decode('utf-8')
-            res["ciphertext"] = b64encode(ciphertext).decode('utf-8')
-            result = json.dumps(res)
-            self.text_output.setText(result)
+            if self.tabs.currentIndex() == 0:
+                res["nonce"] = b64encode(nonce).decode('utf-8')
+                res["ciphertext"] = b64encode(ciphertext).decode('utf-8')
+                result = json.dumps(res)
+                self.text_output.setText(result)
+            else:
+                with open(self.out_file1.text(), 'wb') as file:
+                    file.write(struct.pack('B', len(nonce)))
+                    file.write(nonce)
+                    file.write(ciphertext)
         else:
             self.show_error_message("Error", "Input must not be empty!")
  
     def decrypt_text(self):
         key = self.get_key()
         try:
-            json_input = self.text_output.toPlainText().encode()
-            b64        = json.loads(json_input)
-            nonce      = b64decode(b64['nonce'])
-            ciphertext = b64decode(b64['ciphertext'])
+            if self.tabs.currentIndex() == 0:
+                json_input = self.text_output.toPlainText().encode()
+                b64        = json.loads(json_input)
+                nonce      = b64decode(b64['nonce'])
+                ciphertext = b64decode(b64['ciphertext'])
+            else:
+                if not self.in_file1.text() == "None" and not self.out_file1.text() == "None":
+                    with open(self.in_file1.text(), 'rb') as file:
+                        data = file.read(1)
+                        nonce_len = int(struct.unpack('B', data)[0])
+                        nonce = file.read(nonce_len)
+                        ciphertext = file.read()
+                else:
+                    return
 
             enc_type = self.select_enc.currentText()
             if enc_type == "chacha20":
@@ -227,7 +315,11 @@ class EncryptionApp(QWidget):
                 cipher    = chacha20poly1305.ChaCha20Poly1305(key)
                 plaintext = cipher.decrypt(nonce, ciphertext, aad=b"header")
 
-            self.text_input.setText(plaintext.decode())
+            if self.tabs.currentIndex() == 0:
+                self.text_input.setText(plaintext.decode())
+            else:
+                with open(self.out_file1.text(), 'wb') as file:
+                    file.write(plaintext)
         except (ValueError, KeyError) as e:
             self.show_error_message("Error", f"Incorrect key: {e}")
 
